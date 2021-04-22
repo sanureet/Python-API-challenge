0c7a918c3d56d82d6b1c3725db5548649d197975
sanureet <sanureetbhullar@gmail.com>
2021-04-22 02:06:13 -0700

    hard

--- Instructions/starter_code/api_keys.py
+++ Instructions/starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "7e8d121613d004b630fdc4bfabd1e8d9"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyCUnftNcQ8A2zZSdiag8WJfY4d6zulj35w"
